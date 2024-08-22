253aa1066f17c87a9a92e58b5ccba570739177d9
asheque33 <ashequrrahman3333@gmail.com>
2024-08-22 09:32:48 +0600

    login user setup successfully

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,43 @@
+import config from "../../config";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { authServices } from "./auth.service";
 
 const register = catchAsync(async (req, res) => {
   const result = await authServices.registerIntoDB(req.body);
+  // ! manually remove password to the api response
+  // await result.save();
 
+  // const userObject = result.toObject();
+  // delete userObject.password;
+  // delete userObject.__v;
   sendResponse(res, {
     success: true,
     statusCode: 201,
     message: "User registered successfully",
     data: result,
+    // data: userObject,
+  });
+});
+const login = catchAsync(async (req, res) => {
+  const { accessToken, refreshToken, userExisted } =
+    await authServices.loginIntoDB(req.body);
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: config.node_env === "production",
+  });
+  // const userResponseData = userExisted.toObject();
+  // delete userResponseData.createdAt,
+  //   delete userResponseData.updatedAt,
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "User logged in successfully",
+    token: accessToken,
+    data: userExisted,
   });
 });
 export const authController = {
   register,
+  login,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,58 @@
+import config from "../../config";
+import { isPasswordMatched } from "../../utils/isPasswordMatched";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 
+import jwt from "jsonwebtoken";
+import { ILoginUser } from "./auth.interface";
+
 const registerIntoDB = async (payload: IUser) => {
+  const userExisted = await User.findOne({ email: payload.email });
+  if (userExisted) {
+    throw new Error("User with this email already exists");
+  }
   const result = await User.create(payload);
+
   return result;
+};
+const loginIntoDB = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+  const userExisted = await User.findOne({ email: payload.email }).select(
+    "+password"
+  );
+
+  if (!userExisted) {
+    throw new Error("User not found");
+  }
+  const passwordMatched = await isPasswordMatched(
+    payload.password,
+    userExisted.password
+  );
+  if (!passwordMatched) {
+    throw new Error("Password mismatch");
+  }
+  const jwtPayload = {
+    email: userExisted.email,
+    role: userExisted.role,
+  };
+  const accessToken = jwt.sign(
+    jwtPayload,
+    config.jwt_access_private_key as string,
+    { expiresIn: config.jwt_access_key_expires_in }
+  );
+  const refreshToken = jwt.sign(
+    jwtPayload,
+    config.jwt_refresh_private_key as string,
+    { expiresIn: config.jwt_refresh_key_expires_in }
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    userExisted,
+  };
 };
 export const authServices = {
   registerIntoDB,
+  loginIntoDB,
 };
